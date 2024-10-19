3a408512656311d44beb8d70b156be866e4bdfaa
Hasindu Udara <151644598+hasinduudara@users.noreply.github.com>
2024-10-19 14:00:33 +0530

    Add Application Icon

--- Tic-Tac-Toe-Game/src/main/java/lk/ijse/gdse/tictactoegame/AppInitializer.java
+++ Tic-Tac-Toe-Game/src/main/java/lk/ijse/gdse/tictactoegame/AppInitializer.java
@@ -2,18 +2,23 @@ package lk.ijse.gdse.tictactoegame;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class AppInitializer extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(this.getClass().getResource("/view/GameView.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        Parent load = FXMLLoader.load(getClass().getResource("/view/GameView.fxml"));
+
         stage.setTitle("Tic Tac Toe Game");
+
+        Scene scene = new Scene(new Group(load));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/game-Icon.png")));
+        stage.setScene(scene);
         stage.show();
     }
 

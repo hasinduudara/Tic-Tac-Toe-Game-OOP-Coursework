a3bd5be7c71db3994ddd64b6bc6be252b236be9f
Hasindu Udara <151644598+hasinduudara@users.noreply.github.com>
2024-10-19 09:04:54 +0530

    Create controller, model, view Package

---
+++ Tic-Tac-Toe-Game-OOP-Coursework/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.gdse.tictactoegameoopcoursework {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.gdse.tictactoegameoopcoursework to javafx.fxml;
+    exports lk.ijse.gdse.tictactoegameoopcoursework;
+}
